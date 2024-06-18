552359947b7fe8e79b94373202749deb511b7b78
Nathan Sindelar <nathansindelar@macbook-pro-3.lan>
2024-06-17 19:34:13 -0500

    Class 2 Studio

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -13,3 +13,37 @@
 // add logic below to verify the weather status is clear
 
 // Verify shuttle launch can proceed based on above conditions
+let date = "Monday 2019-03-18";
+let time = "10:05:34 AM";
+let astronautCount = 7;
+let astronautStatus = "Ready";
+let averageAstronautMassKg = 80.7;
+let crewMassKG = astronautCount * averageAstronautMassKg;
+let fuelMassKg = 760000;
+let shuttleMassKg = 74842.31;
+let totalMassKG = crewMassKG + fuelMassKg + shuttleMassKg;
+let maximumMassLimit = 850000;
+let fuelTempCelcius = -225;
+let minimumFuelTemp = -300;
+let maximumFuelTemp = -150;
+let fuelLevel = "100%";
+let weatherStatus = "Clear";
+let preparedForLiftOff = true;
+
+if (astronautCount <= 7 && astronautStatus === "Ready" && totalMassKG < maximumMassLimit && fuelTempCelcius > minimumFuelTemp && fuelTempCelcius < maximumFuelTemp && fuelLevel === "100%" && weatherStatus === "Clear"){
+    preparedForLiftOff === true;
+    console.log("Launch conditions achieved. All systems go.");
+    console.log("Date: ", date);
+    console.log("Time: ", time);
+    console.log("Astronaut Count: ", astronautCount);
+    console.log("Crew Mass KG: ", crewMassKG);
+    console.log("Fuel Mass KG: ", fuelMassKg);
+    console.log("Shuttle Mass KG: ", shuttleMassKg);
+    console.log("Total Mass KG: ", totalMassKG);
+    console.log("Fuel Temp (C): ", fuelTempCelcius);
+    console.log("Weather Status: ", weatherStatus);
+    console.log("Godspeed, Astronauts!");
+} else {
+    console.log("ERROR: Launch conditions not met. Launch Scrubbed");
+}
+
