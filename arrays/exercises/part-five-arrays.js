f8dfbe68a1ba0fbac7845ed16729a45a136e30cd
Nathan Sindelar <nathansindelar@MacBook-Pro-3.local>
2024-06-21 09:21:23 -0500

    Lots and lots of arrays + meat

--- arrays/exercises/part-five-arrays.js
+++ arrays/exercises/part-five-arrays.js
@@ -2,10 +2,20 @@ let str = 'In space, no one can hear you code.';
 let arr = ['B', 'n', 'n', 5];
 
 //1) Use the split method on the string to identify the purpose of the parameter inside the ().
-
+let newArr = [];
+newArr = str.split(' ');
+console.log(newArr);
 //2) Use the join method on the array to identify the purpose of the parameter inside the ().
-
+let newString = '';
+newString = arr.join('');
+console.log(newString);
 //3) Do split or join change the original string/array?
-
+console.log(str);
+console.log(arr);
+console.log("No.");
 //4) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
 let cargoHold = "water,space suits,food,plasma sword,batteries";
+let arr2 = [];
+arr2 = cargoHold.split(',').sort().join(', ');
+console.log(arr2);
+
