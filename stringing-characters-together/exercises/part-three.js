ba536c7546ceaee77f4759e463ab44abda4f24b2
Nathan Sindelar <nathansindelar@macbook-pro-3.lan>
2024-06-21 06:28:32 -0500

    I guess I forgot to push these

--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -1,6 +1,15 @@
 //Part Three section one
 
 let language = 'JavaScript';
+console.log(language.slice(0,1) + language.slice(4,5));
+
+console.log(language.replace("JavaScript", "J") + language.replace("JavaScript", "S"));
+
+console.log(`The abbreviation for "JavaScript" is ${language[0] + language[4]}.`);
+
+let fruit = "Bananas";
+
+console.log(`I like it better when people say "na${fruit.slice(2,7).toLowerCase().replace("s", "z")}" instead of "${fruit}."`);
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
 
@@ -15,3 +24,5 @@ let language = 'JavaScript';
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+
+console.log(notTitleCase.replace("t", "T").replace("c", "C"));
