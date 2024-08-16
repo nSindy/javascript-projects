213a911c24e1fa494c6860be2e0f19d0223ebd12
Nathan Sindelar <ntsindelar@gmail.com>
2024-08-16 12:05:02 -0500

    dom exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,8 +3,29 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
+    button.addEventListener("click", function() {
+        document.getElementById("statusReport").innerHTML = "Houston, we have liftoff!";
+    })
     
+  
+    missionAbort.addEventListener("mouseover", function() {
+    missionAbort.style.backgroundColor = "red";
+    })
+
+    missionAbort.addEventListener("mouseout", function() {
+        missionAbort.style.backgroundColor = "";
+    })
+
+    missionAbort.addEventListener("click", function() {
+       let response = window.confirm("Are you sure you want to abort the mission?");
+        if (response) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+        } 
+    })
+
+
+
+
 }
 
 window.addEventListener("load", init);
